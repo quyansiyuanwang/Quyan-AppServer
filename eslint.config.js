a542ba3995abb7d39d265f73104bbb5d4b78bf5d
quyansiyuanwang <2301179475@qq.com>
2026-07-03 15:34:17 +0800

    chore(config): add commitlint and cspell configuration

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from '@eslint/js'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,cjs,ts}'],
+  },
+  {
+    name: 'app/files-to-ignore',
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/dist-ssr/**',
+      '**/coverage/**',
+      '**/*.local',
+      'pnpm-lock.yaml',
+    ],
+  },
+  js.configs.recommended,
+]
